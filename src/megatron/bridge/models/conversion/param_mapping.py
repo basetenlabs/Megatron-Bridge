3eae4785070edd4642908c9eb5735098c6ddbda4
Jerry Hong <jerry.hong@baseten.co>
2026-08-21 14:16:13 -0700

    [baseten] conversion: tp2 squeeze note (Bridge conflict 2)

--- src/megatron/bridge/models/conversion/param_mapping.py
+++ src/megatron/bridge/models/conversion/param_mapping.py
@@ -815,7 +815,7 @@ class MegatronParamMapping(ABC, Generic[WeightType]):
             else:
                 weights_dict[param_name] = gathered_weights[i].unsqueeze(0)
         for param_name in weights_dict:
-            weights_dict[param_name] = weights_dict[param_name].squeeze()
+            weights_dict[param_name] = weights_dict[param_name].squeeze()  # b10 tp2 note
         return weights_dict
 
     def gather_from_ep_ranks_scale(
